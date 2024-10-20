18d926f9a303f8c45851c616e3f5d2815c110050
RexKeyChain <lazarobasti8@gmail.com>
2024-10-20 13:19:14 +0800

    Update app.py

--- cybersecurity_project/backend/app.py
+++ cybersecurity_project/backend/app.py
@@ -1,7 +1,9 @@
+
 from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 from flask_bcrypt import Bcrypt
 from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
+
 import os
 import secrets
 
@@ -11,6 +13,7 @@ secureKey = secrets.token_hex(64)
 jwtKey = secrets.token_hex(64)
 
 dbPath = os.path.join(os.path.dirname(__file__), 'transactions.db')
+print(dbPath)
 
 #line 14 not done, ewan ko pa pano i link url to this
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + dbPath
@@ -23,8 +26,6 @@ jwt = JWTManager(app)
 
 class User(dBase.Model):
     id = dBase.Column(dBase.Integer, primary_key = True)
-    username = dBase.Column(dBase.String(150), unique=True, nullable=False)
-    password = dBase.Column(dBase.String(150), nullable=False)
 
 @app.route('/')
 def home():
@@ -35,11 +36,11 @@ def signup():
     data = request.get_json()
     username = data['username']
     password = data['password']
-
+    
     user = User.query.filter_by(username = username).first()
     if user:
         return jsonify({"Error:" "This user already exists"}), 400
-    
+    #makes a unique password hash for each user in case of same passwords different users
     hashed_password = bCrypt.generate_pasword_hash(password).decode('utf-8')
     new_user = User(username=username, password=hashed_password)
     dBase.session.add(new_user)
@@ -54,10 +55,10 @@ def login():
     password = data['password']
 
     user = User.query.filter_by(username = username).first()
-
+    #checks if username is valid or if the password matches the one with the hash assigned to the username
     if not user or not bCrypt.check_password_hash(user.password, password):
         return jsonify({"error": "Invalid user or password"}), 401
-    
+    #no clue ano to rn
     accessToken = create_access_token(identity = username)
     return jsonify({"Access token": accessToken}), 200
     
